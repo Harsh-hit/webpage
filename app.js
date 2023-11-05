f6c0654b7af4a19e5dd45455912c03b30259c9da
Harshith <harshithgandavall@gmail.com>
2023-11-05 17:40:38 +0530

    made a lot of changes

--- app.js
+++ app.js
@@ -50,4 +50,61 @@ function scrollToImage(imageId) {
         return false; // Prevent the default link behavior
     }
 }
+var cards = document.getElementsByClassName('card1'),
+    transforms = [
+  { x: 0,
+    z: 0,
+    scale: 1,
+    opacity: 1
+  },
+  { x: '-55%',
+    z: '-50px',
+    scale: 0.8,
+    opacity: 0.8
+  },
+        {
+    x: '-110%',
+    z: '-100px',
+    scale: 0.6,
+    opacity: 0.6
+  },
+        {
+    x: '110%',
+    z: '-100px',
+    scale: 0.6,
+    opacity:0.6
+  },
+  {
+    x: '55%',
+    z: '-50px',
+    scale: 0.8,
+    opacity: 0.8
+  }
+
+
+];
+
+var nextTransform = function(x) {
+  if (x >= cards.length - 1) {
+    x = 0;
+  } else {
+    x++;
+  }
+  return x;
+};
+
+function next() {
+  for (i = 0; i < cards.length; i++) {
+    cards[i].style.transform = 
+      'translateX(' + transforms[nextTransform(i)].x + ') ' + 
+      'translateZ(' + transforms[nextTransform(i)].z + ') ' +
+      'scale(' + transforms[nextTransform(i)].scale + ')';
+    cards[i].style.opacity = transforms[nextTransform(i)].opacity;
+  }
+  transforms.push(transforms.shift());
+}
+  
+document.getElementById('carousel').onclick = function() {
+  next();  
+};
 
